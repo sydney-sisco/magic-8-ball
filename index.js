ad6553ba4565cd1e35e5ff7855e36168b72ef66a
Sydney Sisco <hello@sydsis.co>
2021-06-02 10:15:08 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const PREFIX = '!8';
+
+const divinations = [
+  'It is certain.',
+  'It is decidedly so.',
+  'Without a doubt.',
+  'Yes – definitely.',
+  'You may rely on it.',
+  'As I see it, yes.',
+  'Most likely.',
+  'Outlook good.',
+  'Yes.',
+  'Signs point to yes.',
+  'Reply hazy, try again.',
+  'Ask again later.',
+  'Better not tell you now.',
+  'Cannot predict now.',
+  'Concentrate and ask again.',
+  'Don\'t count on it.',
+  'My reply is no.',
+  'My sources say no.',
+  'Outlook not so good.',
+  'Very doubtful.'
+];
+
+const scry = () => {
+  return divinations[Math.floor(Math.random() * divinations.length)];
+};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  
+  // ignore messages sent by bots
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content.startsWith(PREFIX)) {
+    message.reply(`🎱 ${scry()} 🎱`);
+  }
+
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
