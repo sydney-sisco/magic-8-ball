4b19f33c2aac7abb2f447ac1aaa83c3859b79a91
Sydney Sisco <hello@sydsis.co>
2022-10-29 15:13:39 -0700

    reenables preferences functionality

--- features/wolfram.js
+++ features/wolfram.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-// var preferences = require('./userPreferences');
+var preferences = require('./userPreferences');
 
 const WolframAlphaAPI = require('wolfram-alpha-api');
 const waApi = WolframAlphaAPI(process.env.WOLFRAM_APPID);
@@ -11,7 +11,7 @@ const addLinkToResult = queryString => {
 const wolframGetShort = function(message) {
   const queryString = message.content.substring(3);
 
-  // const userPreferences = preferences.getPreference(`${message.author.username}#${message.author.discriminator}`);
+  const userPreferences = preferences.getPreference(`${message.author.username}#${message.author.discriminator}`);
   // console.log('got:', userPreferences);
 
   const result = waApi.getShort(  {
@@ -25,7 +25,7 @@ const wolframGetShort = function(message) {
 const wolframGetFull = function (message) {
   const queryString = message.content.substring(3);
 
-  // const userPreferences = preferences.getPreference(`${message.author.username}#${message.author.discriminator}`);
+  const userPreferences = preferences.getPreference(`${message.author.username}#${message.author.discriminator}`);
   // console.log('got:', userPreferences);
 
   const result = waApi.getFull({
